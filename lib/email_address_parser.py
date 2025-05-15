ea7e309ad654486a8052cc0083d10b0406e0c3c0
David Amedi <amukoahdavid@gmail.com>
2025-05-15 10:03:10 +0300

    Refactor email address parser implementation and update Pipfile.lock for dependency versions

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,20 @@
 # your code goes here!
+
+import re
+
+class EmailAddressParser:
+    def __init__(self, addresses: str):
+        self.addresses = addresses
+
+    def parse(self):
+        # Split by commas or whitespace
+        parts = re.split(r'[, ]+', self.addresses)
+
+        # Simple regex for validating emails
+        email_pattern = re.compile(r'^[\w\.-]+@[\w\.-]+\.\w+$')
+
+        # Filter valid emails only
+        valid_emails = [email for email in parts if email_pattern.match(email)]
+
+        # Return unique sorted emails
+        return sorted(set(valid_emails))
